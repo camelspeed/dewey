5f4208d1302103583d1b853b9c53a28c179a26f4
Johnny Mohseni <jmohseni@gmail.com>
2016-03-31 16:53:28 -0400

    added the virtualenv concept although not checked in and added another route

--- dewey.py
+++ dewey.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hell():
-    return "Hello World!"
+def index():
+    return "Hello Dr. Falken! Do you want to play a game?"
+
+@app.route("/<name>")
+def greeting(name):
+    return 'Hello %s! Do you want to play a game?' % name
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
