cd5bed27cdcd47c35b2c04ce322322d6d0031c2b
Johnny Mohseni <jmohseni@gmail.com>
2016-04-01 11:39:47 -0400

    adding Travis CI to the project and some more endpoints to test what happens with HTTP Error Codes

--- dewey.py
+++ dewey.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,18 @@ def index():
 @app.route("/<name>")
 def greeting(name):
     return 'Hello %s! Do you want to play a game?' % name
+
+@app.route("/401")
+def error401():
+    abort(401)
+
+@app.route("/404")
+def error404():
+    abort(404)
+
+@app.route("/500")
+def error500():
+    abort(500)
 
 if __name__ == "__main__":
     app.debug = True
